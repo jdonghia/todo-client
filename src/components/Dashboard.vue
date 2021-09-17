98866dd64dbf9a12ffe4b750aa23115268121614
João Donghia <joao.donghia@gmail.com>
2021-09-17 18:32:04 -0300

    feat(components): create dashboard component
    created: Dashboard.vue

---
+++ src/components/Dashboard.vue
@@ -0,0 +1,24 @@
+<template>
+  <header>
+    <Navbar @createTask="createTask"></Navbar>
+    <Tasks ref="tasks"></Tasks>
+  </header>
+</template>
+<script lang="ts">
+import Navbar from "./shared/Navbar.vue";
+import Tasks from "./Tasks.vue";
+import Vue from "vue";
+
+export default Vue.extend({
+  components: {
+    Navbar,
+    Tasks,
+  },
+
+  methods: {
+    createTask(): void {
+      (this.$refs.tasks as any).insertTask(); // how can i type this?
+    },
+  },
+});
+</script>
